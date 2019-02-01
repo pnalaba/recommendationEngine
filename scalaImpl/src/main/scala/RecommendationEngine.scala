667a18b89d4175edbcb7cd3bc30e65172fca95f9
Pradeep Nalabalapu <pradeep.nalabalapu@gmail.com>
2019-02-01 12:49:28 -0800

    moved similarity related functions to the top

--- scalaImpl/src/main/scala/RecommendationEngine.scala
+++ scalaImpl/src/main/scala/RecommendationEngine.scala
@@ -3,10 +3,40 @@ import org.apache.spark.sql.{SparkSession, Column}
 import org.apache.spark.sql.functions._
 import scala.math.log
 import scala.collection.JavaConverters._
+import org.apache.spark.sql.Row
+import org.apache.spark.rdd.RDD
+import scala.collection.mutable.WrappedArray
 
 
 object RecommendationEngine {
+
 	def main(args: Array[String]){
+		//shannon entropy
+		def xlogx(v: Long) : Double = if (v==0) 0 else v*log(v)
+
+		//xlogx(sum) - sum(xlogx)
+		def entropy(k: Seq[Long]) : Double =  {
+		 	(xlogx(k.sum) - k.map(v => xlogx(v)).sum)
+		}
+
+		// lrr= -2* (H(k) - H(rowSums(k)) - H(colSums(k)))
+		val lrr_fn = (num_users: Long,  num_users_R: Long, num_common_users: Long, num_total_users: Long) => {
+			val k : Seq[Long]  = Seq(num_common_users,
+				num_users_R-num_common_users,
+				num_users - num_common_users,
+				num_total_users - num_users-num_users_R+num_common_users)
+			val rowSums : Seq[Long] = Seq(k(0)+k(1),k(2)+k(3))
+			val colSums : Seq[Long] = Seq(k(0)+k(2),k(1)+k(3))
+
+			val rowEntropy = entropy(rowSums)
+			val colEntropy = entropy(colSums)
+			val matrixEntropy = entropy(k)
+
+			val lrr = (rowEntropy + colEntropy - matrixEntropy)*2.0
+			if ( lrr < 0 ) 0.0 else lrr
+		}
+		
+
 		val spark = SparkSession.builder().appName("RecommendationEngine").getOrCreate()
 		spark.conf.set("spark.sql.crossJoin.enabled", true)
 		import spark.implicits._
@@ -20,6 +50,34 @@ object RecommendationEngine {
 		dataDF.show(5)
 
 		val num_total_users = dataDF.agg(countDistinct($"user") as "count" ).collect()(0).getLong(0)
+
+		def get_similarity_fn(num_total_users : Long) = 
+			(num_users: Long,  num_users_R: Long, num_common_users: Long) => {
+			val lrr = lrr_fn(num_users, num_users_R, num_common_users, num_total_users)
+			1.0 - 1.0/(1.0 + lrr)
+		}
+
+
+
+
+/*
+
+		def testUdf = udf( (array: WrappedArray[Int]) => array.toList.flatMap(i => array.toList.map(j => (i,j) -> 1).toMap))
+
+		val userItemsRDD : RDD[Row] = dataDF.groupBy("user").
+			agg(collect_set("movie") as "movies").
+			drop("user").
+			withColumn("movies", testUdf(col("movies"))).
+			rdd
+			*/
+
+
+
+
+		
+
+
+
 		
 		//---------   Convert to "set of users" for each movie    -----------
 
@@ -38,38 +96,10 @@ object RecommendationEngine {
 		val common_set_size_fn =  (s1: Seq[String],s2: Seq[String]) => {(s1.toSet.intersect(s2.toSet)).size}
 		val common_set_size_udf = udf(common_set_size_fn)
 
-		//shannon entropy
-		def xlogx(v: Long) : Double = if (v==0) 0 else v*log(v)
-
-		//xlogx(sum) - sum(xlogx)
-		def entropy(k: Seq[Long]) : Double =  {
-		 	(xlogx(k.sum) - k.map(v => xlogx(v)).sum)
-		}
-
-		// lrr= 2* (H(k) - H(rowSums(k)) - H(colSums(k)))
-		val lrr_fn = (num_users: Long,  num_users_R: Long, num_common_users: Long) => {
-			val k : Seq[Long]  = Seq(num_common_users,
-				num_users_R-num_common_users,
-				num_users - num_common_users,
-				num_total_users - num_users-num_users_R+num_common_users)
-			val rowSums : Seq[Long] = Seq(k(0)+k(1),k(2)+k(3))
-			val colSums : Seq[Long] = Seq(k(0)+k(2),k(1)+k(3))
-
-			val rowEntropy = entropy(rowSums)
-			val colEntropy = entropy(colSums)
-			val matrixEntropy = entropy(k)
-
-			val lrr = (rowEntropy + colEntropy - matrixEntropy)*2.0
-			if ( lrr < 0 ) 0.0 else lrr
-		}
-
-		val similarity_fn = (num_users: Long,  num_users_R: Long, num_common_users: Long) => {
-			val lrr = lrr_fn(num_users, num_users_R, num_common_users)
-			1.0 - 1.0/(1.0 + lrr)
-		}
-		val similarity_udf = udf(similarity_fn)
 
 		// -----  Calculate the similarity using log-likelihood ratio  ----------------
+
+		val similarity_udf = udf(get_similarity_fn(num_total_users))
 		val join = df.
 			join(df.toDF(df.columns.map(_+"_R"):_*), $"movie" < $"movie_R").
 			withColumn("num_common",common_set_size_udf($"users",$"users_R")).
